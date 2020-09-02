754a24e536bcb58294f9700e9dcc1ea63daf58c1
StatsWiz <69688181+StatsWiz00@users.noreply.github.com>
2020-09-02 10:32:00 -0500

    Update TV.java

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -1,12 +1,12 @@
 package taller3.televisores;
 
 public class TV {
-    private Marca marca = null;
+    private Marca marca;
     private int canal = 1;
     private int precio = 500;
     private boolean estado;
     private int volumen = 1;
-    private Control control = null;
+    private Control control;
     private static int numtv;
 
     public TV(Marca marca, boolean estado) {
